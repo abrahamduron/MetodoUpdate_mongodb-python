8206a75e2666b5f310c36ad2fc2ea58d1d3712e2
abrahamduron <abraham.duron@unah.hn>
2023-02-18 00:57:16 -0600

    Método Update para actualizar los registros en mongodb

---
+++ app.py
@@ -0,0 +1,18 @@
+from classes import Estudiante, DbMongo
+from dotenv import load_dotenv
+
+def main():
+    estudiante =Estudiante("Sheryl", "Montoya", "96904707")
+    #estudiante.save()
+    estudiante.nombre = "Esmeralda"
+    estudiante.apellido = "Castro"
+    estudiante.telefono = "85105235"
+    estudiante.update('63e7d6c38fe491575b2f3f7f')
+    
+if __name__ == "__main__":
+    load_dotenv()
+    main()
+
+
+
+
---
+++ classes/Estudiante.py
@@ -0,0 +1,31 @@
+from classes.DbMongo import DbMongo
+from bson.objectid import ObjectId
+class Estudiante:
+
+    def __init__(self, nombre, apellido, telefono):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.__collection = "estudiante"
+
+    def save(self):
+        client, db = DbMongo.getDB()
+        collection = db[self.__collection]
+        collection.insert_one(self.__dict__)
+        client.close()
+        
+    def update(self, id):
+        client, db = DbMongo.getDB()
+        collection = db[self.__collection]
+        resp = collection.update_one(  
+        {
+        '_id': ObjectId(id)
+        }, 
+        {
+            '$set': {
+                "nombre": self.nombre,
+                "apellido": self.apellido,
+                "telefono": self.telefono,
+            }
+        })
+        return resp.modified_count 
